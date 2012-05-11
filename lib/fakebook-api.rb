94bb0c3b2fc3464a98dff1818cdbb76ac2bbb3a2
Stewart Matheson <stew@rtmatheson.com>
2012-05-11 18:11:30 +1000

    Updated generic oath token accessor

--- lib/fakebook-api.rb
+++ lib/fakebook-api.rb
@@ -1,1 +1,9 @@
+require 'fakebook-api/facebook_object'
+require 'fakebook-api/dsl'
 require 'fakebook-api/friends'
+
+module FakebookAPI
+  def self.access_token 
+    "1122334455667788990011223344556677889900"
+  end
+end
