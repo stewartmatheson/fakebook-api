8e247826fd410646f3b769e67a680ce50fc4d78f
Stewart Matheson <stew@rtmatheson.com>
2013-01-02 22:37:24 +1100

    Refactor. Removed generation logic out.

--- generators/facebook_object_generator.rb
+++ generators/facebook_object_generator.rb
@@ -1,5 +1,7 @@
 require 'scrapi'
 require 'mechanize'
+require 'json'
+require 'object_instance_generator'
 
 class FacebookObjectGenerator
 
@@ -7,43 +9,42 @@ class FacebookObjectGenerator
     @fb_email = fb_email
     @fb_pass = fb_pass
 
-    #get the body HTML of the facebook page that needs to be scraped
-    body = facebook_documentation_body
+    #run the the scraper
+    scraped_facebook_objects = facebook_object_scraper.scrape(document_body)
 
-    facebook_object_scraper = Scraper.define do
-      array :facebook_objects
-      process "div#bodyText.bodyText li", :facebook_objects => :text
-      result :facebook_objects
-    end
-
-    scraped_facebook_objects = facebook_object_scraper.scrape(body)
-
+    #iterate the scraped objects
     scraped_facebook_objects.each do |scraped_facebook_object|
-      puts scraped_facebook_object
-      add_object(scraped_facebook_object.split(':').first) if is_facebook_object(scraped_facebook_object.split(':').first)
+
+      #lets pick the url from the string as we will need to fetch the sample object
+      sample_url = scraped_facebook_object.match(/https?:\/\/[\S]+/).to_s
+
+      #now lets get the name
+      object_name = scraped_facebook_object.split(':').first
+
+      if(is_facebook_object(object_name))
+        FacebookObjectInstanceGenerator.new(object_name, sample_url)
+        puts "Cretaed facebook object : #{object_name}"
+      else
+        puts "Can't create a facebook object : #{object_name}"
+      end
     end
   end
   
   private
 
+  def facebook_object_scraper
+    Scraper.define do
+      array :facebook_objects
+      process "div#bodyText.bodyText li", :facebook_objects => :text
+      result :facebook_objects
+    end
+  end
+
   def is_facebook_object scraped_facebook_object
     !(scraped_facebook_object.match(/\W/))
   end
 
-  def add_object object_name
-    template_path = File.expand_path(File.join(File.dirname(__FILE__), "templates"))
-    facebook_object_template = File.open("#{template_path}/fakebook_object.rb.erb")
-    current_template =  ERB.new(facebook_object_template.read)
-    facebook_object_template.close
-
-    target_path = File.expand_path(
-      File.join(File.dirname(__FILE__), "..", "lib", "fakebook-api", "facebook_collections", object_name.downcase + ".rb")
-    )
-
-    File.open(target_path, "w") { |f| f.write(current_template.result(binding)) } 
-  end
-
-  def facebook_documentation_body
+  def document_body
     facebook_login_page = "http://www.facebook.com/login.php?next=http%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Freference%2Fapi%2F"
     agent = Mechanize.new
     page = agent.get(facebook_login_page)
